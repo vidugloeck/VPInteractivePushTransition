36037152dfd0873a6f0e218a0ae14e0dc97ca4ee
Vidu Pirathaparajah <writetovidu@gmail.com>
2013-12-03 04:09:47 +0100

    Initial Commit

---
+++ VPInteractivePushTransitionTests/VPInteractivePushTransitionTests.m
@@ -0,0 +1,34 @@
+//
+//  VPInteractivePushTransitionTests.m
+//  VPInteractivePushTransitionTests
+//
+//  Created by Vidu Pirathaparajah on 03/12/13.
+//  Copyright (c) 2013 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VPInteractivePushTransitionTests : XCTestCase
+
+@end
+
+@implementation VPInteractivePushTransitionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
